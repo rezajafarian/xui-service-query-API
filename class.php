fa3a5fe3feeffabb1ad607e5e22a3c95d5838160
Reza Jafarian <112341834+rezajafarian@users.noreply.github.com>
2023-03-30 15:29:26 +0330

    Update class.php

--- class.php
+++ class.php
@@ -32,15 +32,16 @@ class subscription_inquiry_xui{
     
     private function request($url, $method = false, array $headers = null, $data = null) {
         
-        $curl = curl_init();
+        $curl = curl_init($url);
         curl_setopt_array($curl, [
-            CURLOPT_URL => $url,
             CURLOPT_POST => $method,
             CURLOPT_POSTFIELDS => $data,
-            CURLOPT_HEADER => false,
-            CURLOPT_HTTPHEADER => $headers,
             CURLOPT_RETURNTRANSFER => true
         ]);
+        
+        if (!is_null($headers)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
         
         $result = json_decode(curl_exec($curl), true);
         curl_close($curl);
@@ -48,25 +49,23 @@ class subscription_inquiry_xui{
         
     }
     
-    public function service_status($input, $type){
+    public function service_status($input, $type) {
         
-        $url = $this->ssl . $this->ip . ':' . $this->port . '/xui/inbound/list';
+        $url = "{$this->ssl}{$this->ip}:{$this->port}/xui/inbound/list";
         $object = self::request($url, true, $this->headers)['obj'];
-        
-        for($i=0;$i<=count($object)-1;$i++){
+
+        foreach($object as $item) {
             
-            if($input == $object[$i][$type]){
-                return $object[$i];
-            }else{
-                for($j=0;$j<=count($object[$i]['clientStats']) - 1;$j++){
-                    if($input == $object[$i]['clientStats'][$j]['email']){
-                        return $object[$i]['clientStats'][$j];
+            if (isset($item[$type]) && $input == $item[$type]) {
+                return $item;
+            } else {
+                foreach ($item['clientStats'] as $stat) {
+                    if (isset($stat['email']) && $input === $stat['email']) {
+                        return $stat;
                     }
                 }
             }
-
         }
-
     }
     
 }
